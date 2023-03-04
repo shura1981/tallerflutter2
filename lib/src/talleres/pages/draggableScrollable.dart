74d123ad3c66aa0b18f452db0ffc6001636fc7ad
Steven Realpe <nutritionmegastorepalmira@gmail.com>
2023-03-04 09:42:31 -0500

    tutorial Modalbottomsheet

--- lib/src/talleres/pages/draggableScrollable.dart
+++ lib/src/talleres/pages/draggableScrollable.dart
@@ -20,6 +20,8 @@ class DraggableScrollabe extends StatefulWidget {
   State<DraggableScrollabe> createState() => _DraggableScrollabeState();
 }
 
+bool show = false;
+
 final List<Food> list = [
   Food(
       title: 'Hamburguer',
@@ -92,6 +94,16 @@ class _DraggableScrollabeState extends State<DraggableScrollabe> {
       appBar: AppBar(
         title: Text('Draggable'),
         backgroundColor: Colors.transparent,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: IconButton(
+                onPressed: () {
+                  showBottomSheet(context);
+                },
+                icon: Icon(Icons.open_in_browser)),
+          )
+        ],
       ),
       body: Stack(
         fit: StackFit.expand,
@@ -100,20 +112,23 @@ class _DraggableScrollabeState extends State<DraggableScrollabe> {
             'https://www.eatthis.com/wp-content/uploads/sites/4/2021/07/pancakes.jpg?quality=82&strip=1',
             fit: BoxFit.cover,
           ),
-          DraggableScrollableSheet(
-            initialChildSize: .2,
-            minChildSize: .1,
-            maxChildSize: .6,
-            expand: false,
-            builder: (context, scrollController) {
-              return ClipRRect(
-                borderRadius: BorderRadius.circular(24),
-                child: Container(
-                    color: Colors.white70,
-                    child: ListFood(controller: scrollController)),
-              );
-            },
-          )
+          if (show)
+            DraggableScrollableSheet(
+              initialChildSize: .2,
+              minChildSize: .1,
+              maxChildSize: .8,
+              expand: false,
+              builder: (context, scrollController) {
+                return ClipRRect(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(24),
+                      topRight: Radius.circular(24)),
+                  child: Container(
+                      color: Colors.white70,
+                      child: ListFood(controller: scrollController)),
+                );
+              },
+            )
         ],
       ),
     );
@@ -128,28 +143,86 @@ class ListFood extends StatelessWidget {
   }) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
-      child: ListView.builder(
-        itemCount: list.length,
-        controller: controller,
-        itemBuilder: (context, index) {
-          final food = list[index];
-          return ListTile(
-            title: Text(
-              food.title,
+    return SingleChildScrollView(
+      controller: controller,
+      child: Container(
+        color: Color.fromARGB(115, 235, 235, 235),
+        child: Column(
+            children: list.map((food) {
+          return Material(
+            color: Colors.transparent,
+            child: InkWell(
+              splashColor: Colors.red,
+              onTap: () => print(food.title),
+              child: ListTile(
+                title: Text(
+                  food.title,
+                ),
+                subtitle: Text(food.description),
+                leading: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.network(
+                      food.image,
+                      fit: BoxFit.cover,
+                      height: 50,
+                      width: 50,
+                    )),
+              ),
             ),
-            subtitle: Text(food.description),
-            leading: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Image.network(
-                  food.image,
-                  fit: BoxFit.cover,
-                  height: 50,
-                  width: 50,
-                )),
           );
-        },
+        }).toList()),
+      ),
+    );
+  }
+}
+
+showBottomSheet(BuildContext context) {
+  return showModalBottomSheet(
+    context: context,
+    // isDismissible: false,
+    enableDrag: true,
+    // isScrollControlled: true,
+    shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
+    builder: (context) => MyBottomSheet(),
+  );
+}
+
+class MyBottomSheet extends StatelessWidget {
+  const MyBottomSheet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: SingleChildScrollView(
+        child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: list.map((food) {
+              return Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  splashColor: Colors.red,
+                  onTap: () {
+                    print(food.title);
+                    Navigator.of(context).pop();
+                  },
+                  child: ListTile(
+                    title: Text(
+                      food.title,
+                    ),
+                    subtitle: Text(food.description),
+                    leading: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.network(
+                          food.image,
+                          fit: BoxFit.cover,
+                          height: 50,
+                          width: 50,
+                        )),
+                  ),
+                ),
+              );
+            }).toList()),
       ),
     );
   }
