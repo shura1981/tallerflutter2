8f2967d92c66ea42d260edd25bf5504853fc8a96
Steven Realpe <nutritionmegastorepalmira@gmail.com>
2023-03-28 11:13:12 -0500

    tab modificado

--- lib/src/pages/tab_bar.dart
+++ lib/src/pages/tab_bar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class TabBarScreen extends StatelessWidget {
-  const TabBarScreen({Key? key}) : super(key: key);
+class TabBarScreen3 extends StatelessWidget {
+  const TabBarScreen3({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +16,7 @@ class TabBarScreen extends StatelessWidget {
             TabBar(
               isScrollable: true,
               tabs: [
-                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.directions_car), text: 'Car'),
                 Tab(icon: Icon(Icons.directions_transit)),
                 Tab(icon: Icon(Icons.directions_bike)),
                 Tab(icon: Icon(Icons.directions_car)),
@@ -48,3 +48,58 @@ class TabBarScreen extends StatelessWidget {
     );
   }
 }
+class TabBarScreen extends StatelessWidget {
+  const TabBarScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 9,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('TabBarScreen'),
+        ),
+        body: Column(
+          children: const [
+            Expanded(
+              child: TabBarView(
+                children: [
+                  Icon(Icons.directions_car),
+                  Icon(Icons.directions_transit),
+                  Icon(Icons.directions_bike),
+                  Icon(Icons.directions_car),
+                  Icon(Icons.directions_transit),
+                  Icon(Icons.directions_bike),
+                  Icon(Icons.directions_car),
+                  Icon(Icons.directions_transit),
+                  Icon(Icons.directions_bike),
+                ],
+              ),
+            )
+          ],
+        ),
+        bottomNavigationBar: const TabBar(
+          isScrollable: true,
+          tabs: [
+            Tab(icon: Icon(Icons.directions_car), text: 'Car'),
+            Tab(icon: Icon(Icons.directions_transit)),
+            Tab(icon: Icon(Icons.directions_bike)),
+            Tab(icon: Icon(Icons.directions_car)),
+            Tab(icon: Icon(Icons.directions_transit)),
+            Tab(icon: Icon(Icons.directions_bike)),
+            Tab(icon: Icon(Icons.directions_car)),
+            Tab(icon: Icon(Icons.directions_transit)),
+            Tab(icon: Icon(Icons.directions_bike)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
