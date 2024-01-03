92f58fe0e1cc573c5147e70a9700728dc0fcc17a
Steven Realpe <nutritionmegastorepalmira@gmail.com>
2024-01-02 21:02:05 -0500

    módulo productos iniciado

---
+++ lib/src/productos/home_productos.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeProductosScreen extends StatelessWidget {
+  const HomeProductosScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HomeProductosScreen'),
+      ),
+      body: Center(
+        child: Text('HomeProductosScreen'),
+      ),
+    );
+  }
+}
